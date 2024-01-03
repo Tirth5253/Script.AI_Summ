1f493e6b8436dbafe919bb18bf6c994620f1ccaf
Tirth Joshi <tirthjoshi5253@gmail.com>
2024-01-04 00:20:45 +0530

    initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,22 +6,14 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
+        <img src={logo} alt='sumz_logo' className='w-20 object-contain rounded-full' />
 
-        <button
-          type='button'
-          onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
-          }
-          className='black_btn'
-        >
-          GitHub
-        </button>
+       
       </nav>
 
       <h1 className='head_text'>
         Summarize Articles with <br className='max-md:hidden' />
-        <span className='orange_gradient '>OpenAI GPT-4</span>
+        <span className='orange_gradient '>Script.AI Summerizer</span>
       </h1>
       <h2 className='desc'>
         Simplify your reading with Summize, an open-source article summarizer
